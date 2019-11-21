10d965874a714396a45103d822cd2eaa28a11ae9
RogerStevence <henryimanov@gmail.com>
2019-11-21 20:31:39 +0200

    LightBulb

---
+++ src/scripts/index.js
@@ -0,0 +1,43 @@
+import '../styles/index.scss';
+
+console.log('webpack starterkit');
+
+function turnOnEvent() {
+    const element = document.getElementById("lightBulbStatus");
+    element.innerHTML = "ON";
+    element.style.color = "black";
+    const header = document.getElementsByTagName("h1");
+    header.forEach(element => {
+        element.style.color = "black";
+    });
+    const icon = document.getElementsByTagName("i");
+    icon.forEach(element => {
+        element.style.color = "#ffeb3b";
+    });
+    const body = document.getElementsByTagName("body");
+    body.forEach(element => {
+        element.style.backgroundColor = "white";
+    });
+}
+
+function turnOffEvent() {
+    const element = document.getElementById("lightBulbStatus");
+    element.innerHTML = "OFF";
+    element.style.color = "white";
+    const header = document.getElementsByTagName("h1");
+    header.forEach(element => {
+        element.style.color = "white";
+    });
+    const icon = document.getElementsByTagName("i");
+    icon.forEach(element => {
+        element.style.color = "white";
+    });
+    const body = document.getElementsByTagName("body");
+    body.forEach(element => {
+        element.style.backgroundColor = "grey";
+    });
+}
+
+
+window.turnOnEvent = turnOnEvent;
+window.turnOffEvent = turnOffEvent;
